a3fef47e1c00b7a8815dc552f6bb2bea1f86ac7d
YigitCanAyaz <yigitcanayaz2000@gmail.com>
2024-05-12 16:10:07 +0300

    Advanced Component Architecture

---
+++ src/app/pages/product-detail/product-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+import { Product } from '../models/product';
+
+@Component({
+  selector: 'app-product-detail',
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.css']
+})
+export class ProductDetailComponent {
+  @Input() productDetail?: Product;
+
+  get isShowProductDetail() {return this.productDetail != undefined};
+
+  
+}
---
+++ src/app/pages/product-list/product-list.component.ts
@@ -0,0 +1,17 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Product } from '../models/product';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+  @Input() productList: Product[] = [];
+
+  @Output() itemSelectedEvent = new EventEmitter<number>();
+
+  itemSelected(id: number) {
+    this.itemSelectedEvent.emit(id);
+  }
+}
